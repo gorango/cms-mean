a50a187c318447c9cf221a8cbd631e79c9f3ed0b
Goran <gospaso@gmail.com>
2016-10-18 05:00:43 -0400

    fix(style): update colors and icons

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -14,8 +14,6 @@
   bootstrapConfig.$inject = ['$compileProvider', '$locationProvider', '$httpProvider', '$logProvider', '$mdIconProvider', '$mdThemingProvider', 'localStorageServiceProvider', 'uiGmapGoogleMapApiProvider'];
   bootstrapRun.$inject = ['$rootScope', '$state', '$templateRequest'];
 
-  var iconUrls = ['cli_cloud_drizzle_sun', 'cli_cloud_drizzle', 'cli_cloud_fog_sun', 'cli_cloud_fog', 'cli_cloud_hail_sun', 'cli_cloud_hail', 'cli_cloud_lightning_sun', 'cli_cloud_lightning', 'cli_cloud_rain_sun', 'cli_cloud_rain', 'cli_cloud_snow_sun', 'cli_cloud_snow', 'cli_cloud_sun', 'cli_cloud', 'cli_cloud_wind_sun', 'cli_cloud_wind', 'cli_compass_east', 'cli_compass_north', 'cli_compass_south', 'cli_compass', 'cli_compass_west', 'cli_degrees_celcius', 'cli_degrees_fahrenheit', 'cli_moon_first_quarter', 'cli_moon_full', 'cli_moon_last_quarter', 'cli_moon_new', 'cli_moon', 'cli_moon_waning_crescent', 'cli_moon_waning_gibbous', 'cli_moon_waxing_crescent', 'cli_moon_waxing_gibbous', 'cli_shades', 'cli_snowflake', 'cli_sun_lower', 'cli_sun_low', 'cli_sunrise', 'cli_sunset', 'cli_sun', 'cli_thermometer_100', 'cli_thermometer_25', 'cli_thermometer_50', 'cli_thermometer_75', 'cli_thermometer', 'cli_thermometer_zero', 'cli_tornado', 'cli_umbrella', 'cli_wind'];
-
   function bootstrapConfig($compileProvider, $locationProvider, $httpProvider, $logProvider, $mdIconProvider, $mdThemingProvider, localStorageServiceProvider, uiGmapGoogleMapApiProvider) {
     $locationProvider.html5Mode({
       enabled: true,
@@ -37,12 +35,6 @@
       })
       .warnPalette('red');
 
-    // $mdIconProvider.iconSet('cli', '/modules/core/client/img/icons/climate-icons.svg');
-    angular.forEach(iconUrls, function(name) {
-      var url = '/modules/core/client/img/icons/' + name + '.svg';
-      $mdIconProvider.icon(name, url, 100);
-    });
-
     localStorageServiceProvider.setPrefix('cms');
 
     uiGmapGoogleMapApiProvider.configure({
@@ -57,14 +49,6 @@
         evt.preventDefault();
         $state.go(to.redirectTo, params, { location: 'replace' });
       }
-    });
-
-    // Pre-fetch icons sources by URL and cache in the $templateCache...
-    // subsequent $templateRequest calls will look there first.
-    // $templateRequest('/modules/core/client/img/icons/climate-icons.svg');
-    angular.forEach(iconUrls, function(name) {
-      var url = '/modules/core/client/img/icons/' + name + '.svg';
-      $templateRequest(url);
     });
   }
 
