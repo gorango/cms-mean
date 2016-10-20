c5095b860e9bbc69de27d8b2a55065967e30b46e
Goran <gospaso@gmail.com>
2016-10-20 06:11:28 -0400

    feat(email): add email module

---
+++ modules/email/client/services/email.client.service.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('core')
+    .factory('EmailService', EmailService);
+
+  EmailService.$inject = (['$http']);
+
+  function EmailService($http) {
+    return {
+      send: function(type, quote) {
+        var payload = {
+          emailType: type,
+          quote: quote,
+          recipient: quote.clientEmail
+        };
+        switch (type) {
+          case 'QUOTE':
+            payload.template = 'quote-email';
+            payload.title = 'Snow Clearing Quote';
+            break;
+          case 'QUOTE_OFFICE':
+            payload.template = 'quote-office-email';
+            payload.recipient = 'sales@clearmysnow.com';
+            payload.title = 'Snow Clearing Quote';
+            break;
+          case 'PURCHASE':
+            payload.template = 'purchase-confirm-email';
+            payload.title = 'Purchase Confirmation';
+            break;
+        }
+        $http.post('/api/email', { payload: payload })
+          .then(_handleEmailResponse, _handleEmailError);
+      }
+    };
+
+    function _handleEmailResponse(res) {
+      console.log(res);
+    }
+
+    function _handleEmailError(err) {
+      console.log(err);
+    }
+  }
+}());
---
+++ modules/email/server/controllers/email.server.controller.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+  config = require(path.resolve('./config/config')),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  nodemailer = require('nodemailer'),
+  async = require('async');
+
+var smtpTransport = nodemailer.createTransport(config.mailer.options);
+
+exports.send = function (req, res, next) {
+  var payload = req.body.payload;
+
+  if (!payload.quote || !payload.template) { return res.send(400); }
+
+  async.waterfall([
+    function (done) {
+      var httpTransport = 'http://';
+      if (config.secure && config.secure.ssl === true) {
+        httpTransport = 'https://';
+      }
+      var baseUrl = req.app.get('domain') || httpTransport + req.headers.host;
+      res.render(path.resolve('modules/email/server/templates/' + payload.template), {
+        quote: payload.quote,
+        appName: config.app.title
+      }, function (err, emailHTML) {
+        done(err, emailHTML);
+      });
+    },
+    // If valid email, send email using service
+    function (emailHTML, done) {
+      var mailOptions = {
+        to: payload.recipient,
+        from: config.mailer.from,
+        bcc: payload.bcc ? [payload.bcc, 'dgtldtlng@gmail.com'] : 'dgtldtlng@gmail.com',
+        subject: payload.title,
+        html: emailHTML
+      };
+      smtpTransport.sendMail(mailOptions, function (err) {
+        if (!err) {
+          res.send({
+            message: 'An email has been sent with confirmation.'
+          });
+        } else {
+          return res.status(400).send({
+            message: 'Failed to send email'
+          });
+        }
+        done(err);
+      });
+    }
+  ], function (err) {
+    if (err) {
+      return next(err);
+    }
+  });
+};
